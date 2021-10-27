1aeaae067d1bd59535134bdcdde8b3364604a912
futuresea <artsiomliaver@gmail.com>
2021-10-28 07:50:55 +0900

    delete local files

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import json
 import os
 from flask import jsonify
 from flask_cors import CORS
+import base64
+import sqlite3
 
 app = Flask(__name__)
 CORS(app)
@@ -38,8 +40,15 @@ def find_hours(json_obj, name):
 
 # get pie chart data in geojson file
 def piechart_data():
-    with open("defis_switzerland.geojson", "r") as read_file:
-        data = json.load(read_file)
+    g = Github("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")
+    repo = g.get_repo("chnuessli/defi_data")
+
+    try:
+        file_content = repo.get_contents("data/json/defis_switzerland.geojson", ref="sha")
+        data = json.loads(file_content.decoded_content.decode())
+    except:
+        file_content = get_blob_content(repo, "main", "data/json/defis_switzerland.geojson")
+        data = json.loads(base64.b64decode(file_content.content))
     json_obj = data["features"]
     pie_data = {}
     amount = len(json_obj)
@@ -64,56 +73,106 @@ def piechart_data():
 
 # get defi counts each geojson file (include "defis_kt" string)
 def barchart_data():
-    current_path = os.path.join(os.path.abspath(os.getcwd()), "json")
+    with open('match.json', encoding="utf8") as f:
+        match_data = json.load(f)
+
+    g = Github("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")
+    repo = g.get_repo("chnuessli/defi_data")
+    contents = repo.get_contents("data/json")
     keyword = 'defis_kt'
     result = {}
     label = []
     bar_data = []
-    for file in os.listdir(current_path):
-        if keyword in file:
-            each_file = os.path.join(current_path, file)
-            with open(each_file, "r") as read_file:
-                data = json.load(read_file)
+    for content_file in contents:
+        if keyword in content_file.name:
+            try:
+                file_content = repo.get_contents(content_file.path)
+                data = json.loads(file_content.decoded_content.decode())
+            except:
+                file_content = get_blob_content(repo, "main", content_file.path)
+                data = json.loads(base64.b64decode(file_content.content))
+
             each_defi = find_defi(data["features"], "Feature")
-            label.append(file.replace("defis_kt_", "").replace(".geojson", ""))
+            match_name = content_file.name.replace("defis_kt_", "").replace(".geojson", "")
+            if match_name not in match_data:
+                label.append(match_name)
+            else:
+                label.append(match_data[match_name])
             bar_data.append(each_defi)
     result["label"] = label
     result["data"] = bar_data
     return result
 
 
+# get line chart data
+def linechart_data():
+    result = {}
+    label = []
+    data = []
+    con = sqlite3.connect('defi_data.db')
+    cur = con.cursor()
+
+    # Insert a row of data
+    cur.execute("""SELECT * FROM (
+                   SELECT * FROM defi_data ORDER BY id DESC LIMIT 7
+                )Var1
+                   ORDER BY id ASC;""")
+
+    rows = cur.fetchall()
+    for row in rows:
+        label.append(row[2])
+        data.append(row[1])
+    # Save (commit) the changes
+    con.commit()
+
+    # We can also close the connection if we are done with it.
+    # Just be sure any changes have been committed or they will be lost.
+    con.close()
+
+    result["label"] = label
+    result["data"] = data
+    return result
+
+
 # get file name counts that include "dispo" string
 def find_dispo():
-    current_path = os.path.join(os.path.abspath(os.getcwd()), "json")
+    g = Github("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")
+    repo = g.get_repo("chnuessli/defi_data")
+    contents = repo.get_contents("data/json")
     keyword = 'dispo'
     result = 0
-    for file in os.listdir(current_path):
-        if keyword in file:
+    for content_file in contents:
+        if keyword in content_file.name:
             result += 1
     return result
+
+
+def get_blob_content(repo, branch, path_name):
+    # first get the branch reference
+    ref = repo.get_git_ref(f'heads/{branch}')
+    # then get the tree
+    tree = repo.get_git_tree(ref.object.sha, recursive='/' in path_name).tree
+    # look for path in tree
+    sha = [x.sha for x in tree if x.path == path_name]
+    if not sha:
+        # well, not found..
+        return None
+    # we have sha
+    return repo.get_git_blob(sha[0])
 
 
 # app route api get method make data
 @app.route('/api', methods=['GET'])
 def fetch_json():
     result_data = {}
-    # fetch json
-    # username = "artsiomliaver"
-    # token = "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"
-    # github_session = requests.Session()
-    # github_session.auth = (username, token)
-
-    # json_url = "https://github.com/chnuessli/defi_data/blob/main/data/json/defis_switzerland.geojson"
-    #
-    # g = Github("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")
-    # # for repo in g.get_user().get_repos():
-    # #     print(repo.name)
-    # repo = g.get_repo("chnuessli/defi_stats")
-    # contents = repo.get_contents("app/frontend")
-    # for content_file in contents:
-    #     print(content_file)
-    with open("defis_switzerland.geojson", "r") as read_file:
-        data = json.load(read_file)
+    g = Github("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")
+    repo = g.get_repo("chnuessli/defi_data")
+    try:
+        file_content = repo.get_contents("data/json/defis_switzerland.geojson", ref="sha")
+        data = json.loads(file_content.decoded_content.decode())
+    except:
+        file_content = get_blob_content(repo, "main", "data/json/defis_switzerland.geojson")
+        data = json.loads(base64.b64decode(file_content.content))
 
     all_defi = find_defi(data["features"], "Feature")
     result_data["all"] = all_defi
@@ -129,6 +188,9 @@ def fetch_json():
 
     pie_data = piechart_data()
     result_data["pie_data"] = pie_data
+
+    line_data = linechart_data()
+    result_data["line_data"] = line_data
 
     return_data = jsonify(result_data)
     return return_data
